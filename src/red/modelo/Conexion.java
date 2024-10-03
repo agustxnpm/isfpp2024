514c8ca715ead806de1860fae60582c6e2beb601
agustxnpm <aguspalqui@hotmail.com>
2024-10-03 11:10:56 -0300

    Implemente la carga de datos

--- src/red/modelo/Conexion.java
+++ src/red/modelo/Conexion.java
@@ -6,34 +6,35 @@ public class Conexion {
 
 	private Equipo equipo1;
 	private Equipo equipo2;
-
-	public Conexion(Equipo equipo1, Equipo equipo2) {
+	private TipoCable tipoCable;
+	public Conexion(Equipo equipo1, Equipo equipo2, TipoCable tipoCable) {
 		super();
 		this.equipo1 = equipo1;
 		this.equipo2 = equipo2;
+		this.tipoCable = tipoCable;
 	}
-
 	public Equipo getEquipo1() {
 		return equipo1;
 	}
-
 	public void setEquipo1(Equipo equipo1) {
 		this.equipo1 = equipo1;
 	}
-
 	public Equipo getEquipo2() {
 		return equipo2;
 	}
-
 	public void setEquipo2(Equipo equipo2) {
 		this.equipo2 = equipo2;
 	}
-
+	public TipoCable getTipoCable() {
+		return tipoCable;
+	}
+	public void setTipoCable(TipoCable tipoCable) {
+		this.tipoCable = tipoCable;
+	}
 	@Override
 	public int hashCode() {
-		return Objects.hash(equipo1, equipo2);
+		return Objects.hash(equipo1, equipo2, tipoCable);
 	}
-
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -43,12 +44,14 @@ public class Conexion {
 		if (getClass() != obj.getClass())
 			return false;
 		Conexion other = (Conexion) obj;
-		return Objects.equals(equipo1, other.equipo1) && Objects.equals(equipo2, other.equipo2);
+		return Objects.equals(equipo1, other.equipo1) && Objects.equals(equipo2, other.equipo2)
+				&& Objects.equals(tipoCable, other.tipoCable);
 	}
-
 	@Override
 	public String toString() {
-		return "Conexion [equipo1=" + equipo1 + ", equipo2=" + equipo2 + "]";
+		return "Conexion [equipo1=" + equipo1 + ", equipo2=" + equipo2 + ", tipoCable=" + tipoCable + "]";
 	}
+
+	
 
 }
--- src/red/modelo/Equipo.java
+++ src/red/modelo/Equipo.java
@@ -8,7 +8,6 @@ import java.util.ArrayList;
 
 import red.excepciones.DireccionIpRepetidaException;
 
-
 public class Equipo {
 
 	private String codigo;
@@ -24,15 +23,14 @@ public class Equipo {
 			TipoEquipo tipoEquipo, int cantPuertos, TipoPuerto tipoPuerto) {
 		super();
 		this.codigo = codigo;
-		this.modelo = modelo;
-		this.marca = marca;
-		this.descripcion = descripcion;
+		this.modelo = (modelo == null || modelo.isEmpty()) ? "Modelo desconocido" : modelo;
+		this.marca = (marca == null || marca.isEmpty()) ? "Marca desconocida" : marca;
+		this.descripcion = (descripcion == null || descripcion.isEmpty()) ? "Sin descripción" : descripcion;
 		this.ubicacion = ubicacion;
 		this.tipoEquipo = tipoEquipo;
 		direccionesIp = new ArrayList<String>();
 		puertos = new ArrayList<Puerto>();
 		agregarPuerto(cantPuertos, tipoPuerto);
-
 	}
 
 	public String getCodigo() {
@@ -109,7 +107,6 @@ public class Equipo {
 		return "Equipo [codigo=" + codigo + ", descripcion=" + descripcion + "]";
 	}
 
-
 	public void agregarPuerto(int cantPuertos, TipoPuerto tipoPuerto) throws IllegalArgumentException {
 
 		if (cantPuertos <= 0)
@@ -146,17 +143,18 @@ public class Equipo {
 		if (!matcher.matches()) {
 			throw new IllegalArgumentException("La direccion IP no es valida");
 		}
-		
+
 		// Si ya existe la IP dentro del equipo
 		if (direccionesIp.contains(ip))
 			throw new DireccionIpRepetidaException("La direccion ip ya existe");
-		
-		direccionesIp.add(ip);
-			
-		
-	}
 
-	// ----------------------clase interna puerto------------------------------------//
+		direccionesIp.add(ip);
+
+	}
+	
+
+	// ----------------------clase interna
+	// puerto------------------------------------//
 	private class Puerto {
 
 		private int cantidadPuertos;
@@ -211,6 +209,7 @@ public class Equipo {
 			return Equipo.this;
 		}
 	}
-	// ---------------------- fin clase interna puerto ---------------------------------//
+	// ---------------------- fin clase interna puerto
+	// ---------------------------------//
 
 }
